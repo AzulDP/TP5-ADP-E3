2e9c90fde57354f4465d42f5d848e5c2840dfdd6
Azul De Pol <azul.depol@gmail.com>
2023-04-15 17:34:02 +0100

    agregue el enunciado

---
+++ js/TP5-ejercicio3.js
@@ -0,0 +1,4 @@
+/* Lista de tareas - Dificultad:  🟢🟡🔴
+3 - Crea una web con bootstrap y js, que contenga un botón input donde se pueda cargar una tarea y un botón que al ser presionado agregue dicha tarea a una lista, cada elemento ingresado en la lista debe poder ser eliminado con un botón creado para ese fin. 
+*/
+
